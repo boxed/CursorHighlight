e2b9ca8b0dbe7d0d362c0deedac39dfa12e9f786
Anders Hovmöller <anders.hovmoller@trioptima.com>
2014-04-29 13:16:58 +0200

    ...

--- Cursor Highlight/main.m
+++ Cursor Highlight/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  Cursor Highlight
-//
-//  Created by Anders Hovmöller on 2011-09-27.
-//  Copyright 2011 TriOptima AB. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
