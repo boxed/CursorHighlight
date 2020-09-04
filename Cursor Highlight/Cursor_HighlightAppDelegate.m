d37da27783da559ab061d0736ee32b77c9da7b93
Anders Hovmöller <anders.hovmoller@trioptima.com>
2020-09-04 10:48:36 +0200

    Work on dark background, highlight clicks

--- Cursor Highlight/Cursor_HighlightAppDelegate.m
+++ Cursor Highlight/Cursor_HighlightAppDelegate.m
@@ -4,22 +4,32 @@
 
 @synthesize window;
 
+const float defaultAlpha = 0.2;
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(timer:) userInfo:nil repeats:YES];
-    [self.window setIsVisible:TRUE];
-    [self.window setLevel:1000];
-    [self.window setOpaque:NO];
-    [self.window setBackgroundColor:[NSColor clearColor]];
-    [self.window setAlphaValue:0.2];
-}
+    [NSTimer scheduledTimerWithTimeInterval:0.01 repeats:YES block:^(NSTimer * _Nonnull timer) {
+        NSPoint p = [NSEvent mouseLocation];
+        p.x -= 25;
+        p.y += 25;
+        [self.window setFrameTopLeftPoint:p];
+    }];
+    
+    [self.window setIsVisible: TRUE];
+    [self.window setLevel: 1000];
+    [self.window setOpaque: NO];
+    [self.window setBackgroundColor: [NSColor clearColor]];
+    [self.window setAlphaValue: defaultAlpha];
 
-- (void)timer:(id)sender
-{
-    NSPoint p = [NSEvent mouseLocation];
-    p.x -= 25;
-    p.y += 25;
-    [self.window setFrameTopLeftPoint:p];
+    [NSEvent addGlobalMonitorForEventsMatchingMask:NSEventMaskLeftMouseDown
+                                           handler:^(NSEvent * _Nonnull event) {
+        [self.window setAlphaValue:1.0];
+    }];
+
+    [NSEvent addGlobalMonitorForEventsMatchingMask:NSEventMaskLeftMouseUp
+                                           handler:^(NSEvent * _Nonnull event) {
+        [self.window.animator setAlphaValue:defaultAlpha];
+    }];
 }
 
 @end
